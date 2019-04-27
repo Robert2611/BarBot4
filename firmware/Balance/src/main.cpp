95b3fccf76ce53b5359786a5ca5d69e3cb206644
Robert Niemöller <robertniemoeller@gmail.com>
2019-04-27 10:11:30 +0200

    Changed to atmega48

--- firmware/Balance/src/main.cpp
+++ firmware/Balance/src/main.cpp
@@ -14,31 +14,13 @@
 #define LED_PIN_G 10
 #define LED_PIN_B 11
 #else
-//actual atmega8
-#define BALANCE_PIN_DATA 4
-#define BALANCE_PIN_CLOCK 5
+//atmega48
+#define BALANCE_PIN_DATA 1
+#define BALANCE_PIN_CLOCK 0
 #define BALANCE_GAIN 128
-#define LED_PIN_R 9
-#define LED_PIN_G 10
-#define LED_PIN_B 11
-// ATMEL ATMEGA8 / ARDUINO
-//
-//                  +-\/-+
-//            PC6  1|    |28  PC5 (AI 5) -SCL-
-//      (D 0) PD0  2|    |27  PC4 (AI 4) -SDA-
-//      (D 1) PD1  3|    |26  PC3 (AI 3)
-//      (D 2) PD2  4|    |25  PC2 (AI 2)
-//      (D 3) PD3  5|    |24  PC1 (AI 1)
-//      (D 4) PD4  6|    |23  PC0 (AI 0)
-//            VCC  7|    |22  GND
-//            GND  8|    |21  AREF
-//            PB6  9|    |20  AVCC
-//            PB7 10|    |19  PB5 (D 13)
-//      (D 5) PD5 11|    |18  PB4 (D 12)
-//      (D 6) PD6 12|    |17  PB3 (D 11) PWM
-//      (D 7) PD7 13|    |16  PB2 (D 10) PWM
-//      (D 8) PB0 14|    |15  PB1 (D 9)  PWM
-//                  +----+
+#define LED_PIN_R 6
+#define LED_PIN_G 5
+#define LED_PIN_B 3
 #endif
 
 #define SERIAL_DEBUG
