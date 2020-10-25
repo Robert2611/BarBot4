27f60bad7960fdf8d6d13b652e3cf62ea982b810
Robert Niemöller <robertniemoeller@gmail.com>
2020-10-25 12:42:50 +0100

    Header click for admin  window improved for touch

--- python/barbotgui/__init__.py
+++ python/barbotgui/__init__.py
@@ -6,6 +6,7 @@ import logging
 import platform
 import sys
 
+
 def is_raspberry():
     if platform.system() != "Linux":
         return False
@@ -16,13 +17,16 @@ def is_raspberry():
         return False
     return "raspberry" in name
 
+
 def set_no_spacing(layout):
     layout.setSpacing(0)
-    layout.setContentsMargins(0,0,0,0)
+    layout.setContentsMargins(0, 0, 0, 0)
+
 
 def css_path():
     script_dir = os.path.dirname(__file__)
     return os.path.join(script_dir, "asset")
+
 
 def qt_icon_from_file_name(fileName):
     script_dir = os.path.dirname(__file__)
@@ -34,70 +38,75 @@ class Keyboard(QtWidgets.QWidget):
     _is_widgets_created = False
     _is_shift = False
     target: QtWidgets.QLineEdit = None
-    def __init__(self, target: QtWidgets.QLineEdit, style = None):
+
+    def __init__(self, target: QtWidgets.QLineEdit, style=None):
         super().__init__()
         self.target = target
         self.setLayout(QtWidgets.QVBoxLayout())
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.update_keys()
-        self.setProperty("class","Keyboard")
+        self.setProperty("class", "Keyboard")
         if style is not None:
             self.setStyleSheet(style)
         if is_raspberry():
             self.setCursor(QtCore.Qt.BlankCursor)
-        #move to bottom of the screen
+        # move to bottom of the screen
         desktop = Qt.QApplication.desktop().availableGeometry()
-        desired = Qt.QRect(Qt.QPoint(0,0), self.sizeHint())
+        desired = Qt.QRect(Qt.QPoint(0, 0), self.sizeHint())
         desired.moveBottomRight(desktop.bottomRight())
         desired.setLeft(desktop.left())
         self.setGeometry(desired)
 
     def update_keys(self):
-	    #first row
+            # first row
         keys = [
-            ["1","!"],["2","\""],["3","§"],["4","$"],["5","%"],
-            ["6","&"],["7","/"],["8","("],["9",")"],["0","ß"]
+            ["1", "!"], ["2", "\""], ["3", "§"], ["4", "$"], ["5", "%"],
+            ["6", "&"], ["7", "/"], ["8", "("], ["9", ")"], ["0", "ß"]
         ]
         if not self._is_widgets_created:
             self.first_row = self.add_row([data[0] for data in keys])
         for index, data in enumerate(keys):
-            self.first_row[index].setText(data[1] if self._is_shift else data[0])
+            self.first_row[index].setText(
+                data[1] if self._is_shift else data[0])
 
-	    #second row
-        keys = ["q","w","e","r","t","z","u","i","o","p"]
+            # second row
+        keys = ["q", "w", "e", "r", "t", "z", "u", "i", "o", "p"]
         if not self._is_widgets_created:
             self.second_row = self.add_row(keys)
         for index, letter in enumerate(keys):
-            self.second_row[index].setText(str.upper(letter) if self._is_shift else letter)
-        
-	    #third row
-        keys = ["a","s","d","f","g","h","j","k","l","ö"]
+            self.second_row[index].setText(
+                str.upper(letter) if self._is_shift else letter)
+
+            # third row
+        keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ö"]
         if not self._is_widgets_created:
             self.third_row = self.add_row(keys)
         for index, letter in enumerate(keys):
-            self.third_row[index].setText(str.upper(letter) if self._is_shift else letter)
+            self.third_row[index].setText(
+                str.upper(letter) if self._is_shift else letter)
 
-        #fourth row
-        keys = ["y","x","c","v","b","n","m","ä","ü"]
+        # fourth row
+        keys = ["y", "x", "c", "v", "b", "n", "m", "ä", "ü"]
         if not self._is_widgets_created:
             self.fourth_row = self.add_row(keys)
         for index, letter in enumerate(keys):
-            self.fourth_row[index].setText(str.upper(letter) if self._is_shift else letter)
-        
-        #last row
+            self.fourth_row[index].setText(
+                str.upper(letter) if self._is_shift else letter)
+
+        # last row
         if not self._is_widgets_created:
             row = QtWidgets.QWidget()
             row.setLayout(QtWidgets.QHBoxLayout())
             barbotgui.set_no_spacing(row.layout())
-            #shift
+            # shift
             button = QtWidgets.QPushButton("▲")
             button.clicked.connect(lambda: self.button_clicked("shift"))
             row.layout().addWidget(button)
-            #space
+            # space
             button = QtWidgets.QPushButton(" ")
             button.clicked.connect(lambda: self.button_clicked(" "))
             row.layout().addWidget(button)
-            #delete
+            # delete
             button = QtWidgets.QPushButton("←")
             button.clicked.connect(lambda: self.button_clicked("delete"))
             row.layout().addWidget(button)
@@ -115,35 +124,39 @@ class Keyboard(QtWidgets.QWidget):
                 self.target.setText(self.target.text()[:-1])
             else:
                 self.target.setText(self.target.text() + content)
-            #reset shift state
+            # reset shift state
             if self._is_shift:
                 self._is_shift = False
                 self.update_keys()
 
     def add_row(self, keys):
-        res =  []
+        res = []
         row = QtWidgets.QWidget()
         row.setLayout(QtWidgets.QHBoxLayout())
         barbotgui.set_no_spacing(row.layout())
         for letter in keys:
             button = QtWidgets.QPushButton(letter)
-            button.clicked.connect(lambda checked, b=button: self.button_clicked(b.text()))
+            button.clicked.connect(
+                lambda checked, b=button: self.button_clicked(b.text()))
             res.append(button)
             row.layout().addWidget(button)
         self.layout().addWidget(row)
         return res
 
+
 class MainWindow(QtWidgets.QMainWindow):
-    db:barbot.Database
-    bot:barbot.StateMachine
+    db: barbot.Database
+    bot: barbot.StateMachine
     recipe_filter: barbot.RecipeFilter
     _current_view = None
     _barbot_state_trigger = QtCore.pyqtSignal()
     _last_idle_view = None
     _keyboard: Keyboard = None
     is_admin = False
-    
-    def __init__(self, _db:barbot.Database, _bot:barbot.StateMachine):
+    _timer: QtCore.QTimer
+    _admin_button_active = False
+
+    def __init__(self, _db: barbot.Database, _bot: barbot.StateMachine):
         super().__init__()
         import barbotgui.views
         self.db = _db
@@ -155,28 +168,29 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.setProperty("class", "MainWindow")
         self.styles = open(os.path.join(css_path(), 'main.qss')).read()
-        self.styles = self.styles.replace("#iconpath#", css_path().replace("\\","/"))
+        self.styles = self.styles.replace(
+            "#iconpath#", css_path().replace("\\", "/"))
         self.setStyleSheet(self.styles)
 
         self.mousePressEvent = lambda event: self.close_keyboard()
 
-        #forward status changed
+        # forward status changed
         self._barbot_state_trigger.connect(self.update_view)
         self.bot.on_state_changed = lambda: self._barbot_state_trigger.emit()
 
-        #remove borders and title bar
+        # remove borders and title bar
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.center.setLayout(QtWidgets.QVBoxLayout())
         set_no_spacing(self.center.layout())
 
-        #header
+        # header
         header = QtWidgets.QWidget()
         header.setLayout(QtWidgets.QGridLayout())
         header.setProperty("class", "BarBotHeader")
-        header.mouseDoubleClickEvent = lambda e: self.header_clicked(e)
+        header.mousePressEvent = lambda e: self.header_clicked(e)
         self.center.layout().addWidget(header, 0)
 
-        #content
+        # content
         self._content_wrapper = QtWidgets.QWidget()
         self._content_wrapper.setLayout(QtWidgets.QGridLayout())
         set_no_spacing(self._content_wrapper.layout())
@@ -184,7 +198,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.update_view()
         self.setFixedSize(480, 800)
-        #show fullscreen on raspberry
+        # show fullscreen on raspberry
         if is_raspberry():
             self.showFullScreen()
             self.setCursor(QtCore.Qt.BlankCursor)
@@ -204,7 +218,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # close software
         button = QtWidgets.QPushButton("Schließen")
-        button.clicked.connect(lambda: QtWidgets.QApplication.instance().quit())
+        button.clicked.connect(
+            lambda: QtWidgets.QApplication.instance().quit())
         container.layout().addWidget(button)
 
         label = QtWidgets.QLabel("PI")
@@ -228,7 +243,17 @@ class MainWindow(QtWidgets.QMainWindow):
         filepath = os.path.join(sys.path[0], "main.py")
         barbot.run_command(filepath)
 
+    def _reset_admin_button(self):
+        self._admin_button_active = False
+
     def header_clicked(self, e):
+        print(self._admin_button_active)
+        if not self._admin_button_active:
+            self._admin_button_active = True
+            # reset the admin button after one second
+            self._timer = QtCore.QTimer(self)
+            self._timer.singleShot(1000, self._reset_admin_button)
+            return
         if not self.bot.is_busy():
             if self.is_admin:
                 self.is_admin = False
@@ -237,14 +262,14 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.set_view(barbotgui.views.AdminLogin(self))
         else:
             view = QtWidgets.QWidget()
-            self.add_system_view(view)           
+            self.add_system_view(view)
             self.set_view(view)
 
     def close_keyboard(self):
         if self._keyboard is not None:
             self._keyboard.close()
             self._keyboard = None
-    
+
     def open_keyboard(self, target: QtWidgets.QLineEdit):
         if self._keyboard is not None:
             self._keyboard.close()
@@ -256,23 +281,23 @@ class MainWindow(QtWidgets.QMainWindow):
         if self._current_view == view:
             logging.debug("View is allready set")
             return
-        #remove existing item from window
+        # remove existing item from window
         if self._current_view is not None:
-            #switch from idle to busy?
+            # switch from idle to busy?
             if isinstance(self._current_view, barbotgui.views.IdleView)\
-                and not isinstance(view, barbotgui.views.IdleView):
-                #just remove it from the visuals
+                    and not isinstance(view, barbotgui.views.IdleView):
+                # just remove it from the visuals
                 self._current_view.setParent(None)
             else:
-                #delete the view
+                # delete the view
                 self._current_view.deleteLater()
         self._current_view = view
-        #save the last used idle view        
+        # save the last used idle view
         if isinstance(view, barbotgui.views.IdleView):
             self._last_idle_view = view
         self._content_wrapper.layout().addWidget(self._current_view)
 
-    def update_view(self, force_reload = False):
+    def update_view(self, force_reload=False):
         if not self.bot.is_busy():
             if self.is_admin:
                 self.set_view(barbotgui.views.AdminOverview(self))
@@ -294,8 +319,8 @@ class MainWindow(QtWidgets.QMainWindow):
         splash.show()
         QtCore.QTimer.singleShot(1000, lambda splash=splash: splash.close())
 
-    def combobox_amounts(self, selectedData = None):
-        #add ingredient name
+    def combobox_amounts(self, selectedData=None):
+        # add ingredient name
         wAmount = QtWidgets.QComboBox()
         wAmount.addItem("-", -1)
         wAmount.setCurrentIndex(0)
@@ -306,9 +331,9 @@ class MainWindow(QtWidgets.QMainWindow):
             i = i+1
         return wAmount
 
-    def combobox_ingredients(self, selectedData = None, only_available = False):
+    def combobox_ingredients(self, selectedData=None, only_available=False):
         ing = self.db.list_ingredients(only_available).values()
-        #add ingredient name
+        # add ingredient name
         wIngredient = QtWidgets.QComboBox()
         wIngredient.addItem("-", -1)
         wIngredient.setCurrentIndex(0)
@@ -320,6 +345,7 @@ class MainWindow(QtWidgets.QMainWindow):
             i = i+1
         return wIngredient
 
+
 class View(QtWidgets.QWidget):
     window: MainWindow
     dab: barbot.Database
@@ -330,6 +356,7 @@ class View(QtWidgets.QWidget):
         self.window = window
         self.db = window.db
         self.bot = window.bot
+
 
 class GlasFilling():
     color: str
@@ -429,9 +456,9 @@ class GlasIndicator(QtWidgets.QLabel):
         # bottom right
         path.quadTo(
             Qt.QPoint(self._top_width/2, self._top_pos +
-                   self._height + self._roundness),
+                      self._height + self._roundness),
             Qt.QPoint((self._top_width+self._bottom_width) /
-                   2, self._top_pos + self._height)
+                      2, self._top_pos + self._height)
         )
         # top right
         path.lineTo(Qt.QPoint(self._top_width, self._top_pos + 0))
