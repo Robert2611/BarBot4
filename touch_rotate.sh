1abbdcf9b149a0416d88178ceb625b4e2d868900
Robert Niemöller <robertniemoeller@gmail.com>
2021-09-13 13:34:06 +0200

    Offset vertical touch position by 1%

--- touch_rotate.sh
+++ touch_rotate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sleep 3
-xinput --set-prop 'raspberrypi-ts' 'Coordinate Transformation Matrix'  0 -1 1 1 0 0 0 0 1
+# 6th coordinate offsets touch in y direction for better usage with touch display
+xinput --set-prop 'raspberrypi-ts' 'Coordinate Transformation Matrix'  0 -1 1 1 0 -0.02 0 0 1
 xrandr --output DSI-1 --rotate left
