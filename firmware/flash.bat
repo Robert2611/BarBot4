253d77ea9752529b2dd8a3bd3446a2e25f79eaa8
Robert Niemöller <36860774+Robert2611@users.noreply.github.com>
2022-06-28 11:57:41 +0200

    Update flash.bat

--- firmware/flash.bat
+++ firmware/flash.bat
@@ -23,7 +23,7 @@ ECHO.
 
 SET batch_path=%~dp0
 SET dude_path="%batch_path%\tool-avrdude\avrdude.exe"
-SET hex_path=%batch_path%\%folder%\.pio\build\atmega_usbasp\firmware.hex
+SET hex_path=%batch_path%\%folder%firmware.hex
 
 "%dude_path%" -c usbasp -p m328p -U hfuse:w:0xDE:m -U lfuse:w:0xFF:m -U efuse:w:0xFD:m -U flash:w:%hex_path%:a
 
