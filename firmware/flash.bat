199f3139825062271e3fee6e973cd0361e0853a2
Robert Niemöller <robertniemoeller@gmail.com>
2022-07-02 10:21:56 +0200

    Firmware flashing for mainboard

--- firmware/flash.bat
+++ firmware/flash.bat
@@ -1,13 +1,14 @@
 @ECHO OFF
 SET batch_path=%~dp0
-ECHO.
-ECHO.
+
+:start
+ECHO -------------
 ECHO B - Balance
 ECHO C - Crusher
 ECHO M - Mainboard
 ECHO S - Straw
 ECHO X - Mixer
-ECHO.
+ECHO -------------
 
 SET /P M=Buchstaben eingeben und ENTER druecken:
 IF %M%==M SET folder=mainboard
@@ -18,8 +19,6 @@ IF %M%==X SET folder=mixer
 
 ECHO %folder% wurde gewaehlt
 ECHO.
-ECHO.
-ECHO.
 
 IF %M%==M GOTO mainboard
 
@@ -27,13 +26,13 @@ IF %M%==M GOTO mainboard
 SET dude_path="%batch_path%\tool-avrdude\avrdude.exe"
 SET hex_path=%batch_path%\%folder%.hex
 "%dude_path%" -c usbasp -p m328p -U hfuse:w:0xDE:m -U lfuse:w:0xFF:m -U efuse:w:0xFD:m -U flash:w:%hex_path%:a
-GOTO end
+GOTO start
 
 :mainboard
-SET esptool_path="%batch_path%\esptool\esptool.exe"
-SET bin_path=%batch_path%\mainboard.bin
-"%esptool_path%" --chip esp32 write_flash -fs 1MB -fm dout 0x0 "%bin_path%"
-GOTO end
+SET /P port=Bitte den COM-Port angeben, an dem der ESP32 angeschlossen ist:
+SET esptool_path="%batch_path%esptool\esptool.exe"
+SET bin_path="%batch_path%mainboard.bin"
+%esptool_path% --port "%port%" --chip esp32 write_flash -fs 1MB -fm dout 0x0 "%bin_path%"
+GOTO start
 
-:end
 PAUSE
