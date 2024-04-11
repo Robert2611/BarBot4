294d5efee0d31d8360b1888ba1ac1b17e83bc06b
Robert Niemöller <robertniemoeller@gmail.com>
2024-04-11 21:10:37 +0200

    Start script improved

--- raspberry/start.sh
+++ raspberry/start.sh
@@ -2,5 +2,6 @@
 # set display, only necessary if called from remote
 [ -z $DISPLAY ] && export DISPLAY=:0
 # get the folder of the currently running file 
-SCRIPT_FOLDER=$(readlink -f "$0"|xargs dirname)
-python3 $SCRIPT_FOLDER/python/main.py
+SCRIPT_FOLDER=$(dirname "$0")
+# start the barbot and forward all arguments 
+python3 $SCRIPT_FOLDER/python/main.py $@
